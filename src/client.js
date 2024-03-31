c06d889322b5c7978d40347d87fcc6a5e2c16ba2
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-31 17:13:20 +0700

    fix: hardcode hostname and change testing mode to false

--- src/client.js
+++ src/client.js
@@ -2,21 +2,26 @@
 
 var AWClient = require("../aw-client-js/out/aw-client.js").AWClient;
 var ua_parser = require("ua-parser-js");
-var retry = require("p-retry") // See aw-watcher-web issue #41
+var retry = require("p-retry"); // See aw-watcher-web issue #41
 
 function emitNotification(title, message) {
   chrome.notifications.create({
-    "type": "basic",
-    "iconUrl": chrome.extension.getURL("media/logo/logo-128.png"),
-    "title": title,
-    "message": message,
+    type: "basic",
+    iconUrl: chrome.extension.getURL("media/logo/logo-128.png"),
+    title: title,
+    message: message,
   });
 }
 
 function logHttpError(error) {
   // No response property for network errors
   if (error.response) {
-    console.error("Status code: " + err.response.status + ", response: " + err.response.data.message);
+    console.error(
+      "Status code: " +
+        err.response.status +
+        ", response: " +
+        err.response.data.message
+    );
   } else {
     console.error("Unexpected error: " + error);
   }
@@ -35,19 +40,22 @@ var client = {
   lastSyncSuccess: true,
   browserName: null,
 
-  setup: function() {
+  setup: function () {
     console.log("Setting up client");
     client.browserName = getBrowserName();
     // Check if in dev mode
-    chrome.management.getSelf(function(info) {
+    chrome.management.getSelf(function (info) {
       client.testing = info.installType === "development";
       console.log("testing: " + client.testing);
 
-      client.awc = new AWClient("aw-client-web", {testing: client.testing});
+      client.awc = new AWClient("aw-client-web", { testing: false });
       client.createBucket();
 
       // Needed in order to show testing information in popup
-      chrome.storage.local.set({"testing": client.testing, "baseURL": client.awc.baseURL});
+      chrome.storage.local.set({
+        testing: client.testing,
+        baseURL: client.awc.baseURL,
+      });
     });
   },
 
@@ -55,43 +63,41 @@ var client = {
     return "aw-watcher-web-" + client.browserName.toLowerCase();
   },
 
-  updateSyncStatus: function(){
+  updateSyncStatus: function () {
     chrome.storage.local.set({
-      "lastSyncSuccess": client.lastSyncSuccess,
-      "lastSync": new Date().toISOString()
+      lastSyncSuccess: client.lastSyncSuccess,
+      lastSync: new Date().toISOString(),
     });
   },
 
-  createBucket: function(){
-    if (this.testing === null)
-      return;
+  createBucket: function () {
+    if (this.testing === null) return;
     // TODO: We might want to get the hostname somehow, maybe like this:
     // https://stackoverflow.com/questions/28223087/how-can-i-allow-firefox-or-chrome-to-read-a-pcs-hostname-or-other-assignable
     var bucket_id = this.getBucketId();
     var eventtype = "web.tab.current";
-    var hostname = "unknown";
+    var hostname = "jeff-laptop";
 
     function attempt() {
-      return client.awc.ensureBucket(bucket_id, eventtype, hostname)
-        .catch( (err) => {
+      return client.awc
+        .ensureBucket(bucket_id, eventtype, hostname)
+        .catch((err) => {
           console.error("Failed to create bucket, retrying...");
           logHttpError(err);
           return Promise.reject(err);
-        }
-      );
+        });
     }
 
     retry(attempt, { forever: true });
   },
 
-  sendHeartbeat: function(timestamp, data, pulsetime) {
-    if (this.testing === null)
-      return;
+  sendHeartbeat: function (timestamp, data, pulsetime) {
+    if (this.testing === null) return;
 
     var payload = {
-        "data": data,
-        "duration": 0.0,
-        "timestamp": timestamp.toISOString(),
+      data: data,
+      duration: 0.0,
+      timestamp: timestamp.toISOString(),
     };
 
     var attempt = () => {
@@ -108,8 +114,9 @@ var client = {
         }
         client.lastSyncSuccess = true;
         client.updateSyncStatus();
-      }, (err) => {
-        if(client.lastSyncSuccess) {
+      },
+      (err) => {
+        if (client.lastSyncSuccess) {
           emitNotification(
             "Unable to send event to server",
             "Please ensure that ActivityWatch is running"
@@ -120,7 +127,7 @@ var client = {
         logHttpError(err);
       }
     );
-  }
+  },
 };
 
 module.exports = client;
